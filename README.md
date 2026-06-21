3fc85fe126650b1d822ba7f9b6570046a1b3da5e
Dan Brown <dbrown.email@gmail.com>
2026-06-21 18:29:36 +0100

    feat(parse-csv): add Jupyter Notebook.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # python-parse-csv
 Parse a CSV file with Python.
+
+Run the CSV parser
+
+```
+pip3 install pandas matplotlib
+python3 main.py
+```
+
+Jupyter Notebook
+
+Install Jupyter extension in VSCode
+
+From the Command Palette (⇧⌘P) run and save the file as main.ipunb:
+```
+Create: New Jupyter Notebook
+```
--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-ftse100Data = pd.read_csv("prices.csv").convert_dtypes()
+ftse_100_data = pd.read_csv("prices.csv", parse_dates=["Date"]).convert_dtypes()
 
 print("First 5 Rows:")
-print(ftse100Data.head())
+print(ftse_100_data.head())
+
+new_column_names = {
+    "Date": "date",
+    "Open Price": "open_price",
+    "Close Price": "close_price",
+    "High Price": "high_price",
+    "Low Price": "low_price",
+    "Volume": "volume",
+}
+
+data = ftse_100_data.rename(columns=new_column_names)
+
+print("Columns:")
+print(data.columns)
+
+print("First 5 rows:")
+print(data.head())
+
+print("Check for missing data:")
+print(data.info())
+
+print("Print location of missing daat:")
+print(data.loc[data.isna().any(axis="columns")])
+
+print("Adding missing data...")
+data = data.rename(columns=new_column_names).combine_first(pd.DataFrame({"low_price": {3: 710032.92}}))
+
+print("First 5 corrected rows:")
+print(data.head())
+
+# Remove duplicates
+data.loc[data.duplicated(keep=False)]
+
+data = data.drop_duplicates()
+
+data.to_csv("prices-cleaned.csv", index=False)
+
+# Create chart
+data = pd.read_csv("prices-cleaned.csv").convert_dtypes()
+
+fig, ax = plt.subplots()
+ax.scatter(data["date"], data["open_price"])
+ax.set_title("Scatter Plot of Open Prices")
+ax.set_xlabel("Date")
+ax.set_ylabel("Open Price")
+fig.show()
